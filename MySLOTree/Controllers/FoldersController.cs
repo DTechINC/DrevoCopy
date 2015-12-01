3091d085007ef1b1cb0ba4346ff329ae29612891
Marat Kazgozhin <marat-tip@mail.ru>
2015-12-01 08:12:06 +0600

    Готовое приложение

---
+++ MySLOTree/Controllers/FoldersController.cs
@@ -0,0 +1,108 @@
+﻿using MySLOTree.Filters;
+using MySLOTree.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySLOTree.Controllers
+{
+    [InitializeSimpleNewsDB]
+    public class FoldersController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (TreeContext context = new TreeContext())
+            {
+                FoldersListModel model = new FoldersListModel()
+                {
+                    Folders = context.Folders.Where(x => !x.IsDeleted).ToArray().Select(x => new FoldersModel(x))
+                };
+
+                return View(model);
+
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(int? parentId, string title)
+        {
+            using (TreeContext context = new TreeContext())
+            {
+                var newFolders = new Folders()
+                {
+                    ParentId = parentId,
+                    Title = title
+                };
+                context.Folders.Add(newFolders);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Move(int nodeId, int? newParentId)
+        {
+            if (nodeId == newParentId)
+            {
+                return RedirectToAction("Index");
+            }
+            using (TreeContext context = new TreeContext())
+            {
+                if (newParentId.HasValue && ContainsChilds(context, nodeId, newParentId.Value))
+                {
+                    return RedirectToAction("Index");
+                }
+                var node = context.Folders.Where(x => x.Id == nodeId).Single();
+                node.ParentId = newParentId;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private bool ContainsChilds(TreeContext context, int parentId, int id)
+        {
+            bool result = false;
+            var inner = context.Folders.Where(x => x.ParentId == parentId && !x.IsDeleted).ToArray();
+            foreach (var node in inner)
+            {
+                if (node.Id == id && node.ParentId == parentId)
+                {
+                    return true;
+                }
+                result = ContainsChilds(context, node.Id, id);
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (TreeContext context = new TreeContext())
+            {
+                DeleteNodes(context, id);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private void DeleteNodes(TreeContext context, int id)
+        {
+            var inner = context.Folders.Where(x => x.ParentId == id && !x.IsDeleted).ToArray();
+            foreach (var node in inner)
+            {
+                node.IsDeleted = true;
+                DeleteNodes(context, node.Id);
+            }
+            var deleted = context.Folders.Where(x => x.Id == id && !x.IsDeleted).Single();
+            deleted.IsDeleted = true;
+        }
+    }
+}
---
+++ MySLOTree/Models/NewsModels.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace MySLOTree.Models
+{
+    public class TreeContext : DbContext
+    {
+        public TreeContext()
+            : base("TreesContext")
+        {
+        }
+
+        public DbSet<Folders> Folders { get; set; }
+    }
+
+    [Table("Folders")]
+    public class Folders
+    {
+        public Folders()
+        {
+            IsDeleted = false;
+        }
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string Title { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class FoldersModel
+    {
+        internal FoldersModel(Folders folders)
+        {
+            this.Id = folders.Id;
+            this.ParentId = folders.ParentId;
+            this.Title = folders.Title;
+        }
+
+        public int Id {get; set; }
+        public int? ParentId {get; set; }
+        public string Title {get;set;}
+    }
+
+    public class FoldersListModel
+    {
+        public int? Seed { get; set; }
+        public IEnumerable<FoldersModel> Folders { get; set; }
+    }
+
+    
+}
